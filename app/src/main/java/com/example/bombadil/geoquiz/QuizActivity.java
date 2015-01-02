917f3ba078194fbfe33e7cea4a76f6b101875f26
bombadil7 <akniazev@gmail.com>
2015-01-02 15:44:26 -0800

    Changed buttons to ImageButtons.  Added Log.d()  statements for debug, but keep them commented out.

--- app/src/main/java/com/example/bombadil/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/bombadil/geoquiz/QuizActivity.java
@@ -2,20 +2,27 @@ package com.example.bombadil.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class QuizActivity extends Activity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mPrevButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
             new TrueFalse(R.string.question_oceans, true),
@@ -25,9 +32,11 @@ public class QuizActivity extends Activity {
             new TrueFalse(R.string.question_asia, true),
     };
 
-    private int mCurrentIndex = 0;
+    private int mCurrentIndex;  // variable is set to zero by default upon creation
 
     private void updateQuestion() {
+//        Log.d(TAG, "Updating question text for question #" + mCurrentIndex,
+//                new Exception());
         int question = mQuestionBank[mCurrentIndex].getQuestion();
         mQuestionTextView.setText(question);
     }
@@ -35,7 +44,7 @@ public class QuizActivity extends Activity {
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
 
-        int messageResId = 0;
+        int messageResId;
 
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
@@ -49,6 +58,7 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) is called");
         setContentView(R.layout.activity_quiz /*Layout resource ID*/); // inflates the layout
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -76,7 +86,7 @@ public class QuizActivity extends Activity {
             }
         });
 
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,7 +95,7 @@ public class QuizActivity extends Activity {
             }
         });
 
-        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,9 +106,53 @@ public class QuizActivity extends Activity {
                 updateQuestion();
             }
         });
+
+        // check if it's a new run of the app or just a screen rotation and
+        // set the index accordingly
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
+    }   // End of onCreate(Bundle)
+
+    // preserve the current location in the quiz through screen rotation
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+//        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+//        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+//        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+//        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+//        Log.d(TAG, "onDestroy() called");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
